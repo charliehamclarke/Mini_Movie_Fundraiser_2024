fb4788eda8819013fae648d795390bd30ed87f43
charliehamclarke <charlieclarke@mmc.school.nz>
2024-06-06 12:03:00 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,16 @@
+# functions go here
+
+
+# main routine goes here`
+while True:
+        want_instructions = input("Do you want to read the instructions? ").lower()
+
+
+        if want_instructions == "yes" or want_instructions == "y":
+             print("Instructions go here")
+        elif want_instructions == "no" or want_instructions == "n":
+             pass
+        else:
+             print("please answer yes / no")
+
+print("we are done")
---
+++ 02_yes_no_V2.py
@@ -0,0 +1,28 @@
+# functions go here
+def yes_no(question):
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+        return "no"
+
+    else:
+    print("please enter yes or no")
+
+
+# main routine goes here`
+
+    want_instructions = yes_no("Do you want to read the instructions? ")
+
+
+    if want_instructions == "yes" or want_instructions == "y":
+         print("Instructions go here")
+    elif want_instructions == "no" or want_instructions == "n":
+         pass
+    else:
+         print("please answer yes / no")
+
+print("we are done")
